46d24aa8de01341b9fcc16e5740c5be87875b80f
Andrey Tarantsov <andreyvit@gmail.com>
2008-08-09 22:21:59 +0700

    Removed debug output from the common mae library.

--- lib/mae-lib-general.sh
+++ lib/mae-lib-general.sh
@@ -7,7 +7,6 @@ test -z "$MAE_DIR" &&
   die "\$MAE_DIR should be set and point to a local MAE directory"
 test -d "$MAE_DIR" ||
   die "\$MAE_DIR points to a non-existent directory $MAE_DIR"
-echo "using local repository at $MAE_DIR"
 
 bin="$base_dir/bin"
 packs_dir="$MAE_DIR/packs"
