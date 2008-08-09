05f2a326ef884aa70f9d12ef1d1528cac5473d8a
Andrey Tarantsov <andreyvit@gmail.com>
2008-08-09 22:01:15 +0700

    mae-ls-pack (and lspack.jar)

---
+++ src/java/lspack/com/yoursway/magicecabu/LsPack.java
@@ -0,0 +1,61 @@
+package com.yoursway.magicecabu;
+
+import java.io.BufferedInputStream;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.util.zip.ZipEntry;
+import java.util.zip.ZipInputStream;
+
+public class LsPack {
+    
+    @SuppressWarnings("serial")
+    static class Exit extends RuntimeException {
+        
+        private final int code;
+        
+        public Exit(int code) {
+            this.code = code;
+        }
+        
+        public void proceed() {
+            System.exit(code);
+        }
+        
+    }
+    
+    public static void main(String[] args) {
+        try {
+            if (args.length != 1) {
+                System.err.println("Usage: java -jar xxxxxx pack.zip");
+                throw new Exit(13);
+            }
+            File pack = new File(args[0]);
+            if (!pack.isFile()) {
+                System.err.println("invalid file: " + pack.getPath());
+                throw new Exit(5);
+            }
+            proceed(pack);
+        } catch (Exit exit) {
+            exit.proceed();
+        }
+    }
+    
+    private static void proceed(File pack) {
+        try {
+            ZipInputStream zin = new ZipInputStream(new BufferedInputStream(new FileInputStream(pack), 1024 * 1024));
+            ZipEntry entry = zin.getNextEntry();
+            while (entry != null) {
+                String sha1 = entry.getName();
+                long size = entry.getSize();
+                System.out.println("B " + sha1 + " " + size);
+                entry = zin.getNextEntry();
+            }
+            zin.close();
+        } catch (IOException e) {
+            System.err.println("lspack: I/O error");
+            throw new Exit(1);
+        }
+    }
+    
+}
